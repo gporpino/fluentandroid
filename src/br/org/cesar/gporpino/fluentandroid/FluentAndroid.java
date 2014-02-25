903fb44d21218ff2d535d84417ec817afa2b3832
Gustavo Porpino <gpc@cesar.org.br>
2014-02-25 10:47:17 -0300

    Implementing select and fluent cursor

--- src/br/org/cesar/gporpino/fluentandroid/FluentAndroid.java
+++ src/br/org/cesar/gporpino/fluentandroid/FluentAndroid.java
@@ -1,5 +1,28 @@
 package br.org.cesar.gporpino.fluentandroid;
 
+import android.content.Context;
+
 public class FluentAndroid {
-	private 
+	private static FluentAndroid instance;
+	private Context mContext;
+	
+	public static FluentAndroid getInstance(){
+		if (instance == null){
+			instance = new FluentAndroid();
+		}
+		return instance;
+	}
+	
+	private FluentAndroid(){
+		
+	}
+	
+	public void init(Context context){
+		mContext = context;
+	}
+	
+	public Context getContext(){
+		return mContext;
+	}
 }
+
--- src/br/org/cesar/gporpino/fluentandroid/database/Select.java
+++ src/br/org/cesar/gporpino/fluentandroid/database/Select.java
@@ -1,5 +1,32 @@
 package br.org.cesar.gporpino.fluentandroid.database;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import br.org.cesar.gporpino.fluentandroid.FluentAndroid;
+
 public class Select {
 
+	private String[] mProjection;
+	private Context mContext;
+	private Uri mUri;
+
+	public Select(String... projection) {
+		mProjection = projection;
+		mContext = FluentAndroid.getInstance().getContext();
+	} 
+	
+	public Select from(Uri uri){
+		mUri = uri;
+		return this;
+	}
+	
+	public FluentCursor execute(){
+		Cursor cursor = mContext.getContentResolver().query(mUri, mProjection, null, null, null);
+		if (cursor != null){
+			return new FluentCursor(cursor);
+		}
+		return null;
+	}
+	
 }
